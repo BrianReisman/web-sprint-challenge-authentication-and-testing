9d000f9b8f4013888d2b8834b82713a0a53aa084
BrianReisman <brianreisman@protonmail.com>
2021-04-18 09:55:16 -0400

    generateToken function implimented

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -30,30 +30,39 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  res.end("implement login, please!");
+  const { password } = req.body;
+  const user = req.body;
+
+  if (!user.password || !user.username) {
+    res.status(400).json({ message: "username and password required" });
+  }
+
+  Model.findByName(req.body).then((user) => {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+      res.status(200).json({
+        message: `Welcome, ${user.username}`,
+        token,
+      });
+    }
+  });
   /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
     4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
       the response body should include a string exactly as follows: "invalid credentials".
   */
 });
+
+function generateToken(user) {
+  const payload = {
+    sub: user.id,
+    username: user.username,
+  };
+  const secret = "shh";
+  const options = {
+    expiresIn: "1h",
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
